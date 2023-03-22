62587bbea2fe7551105021d5cabc8b10b28665ad
Gulasal Kuchkarova <gulasalk424@gmail.com>
2023-03-22 08:09:47 +0100

    create editable image component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { NextUIProvider } from "@nextui-org/react";
 import NavbarComponent from "./components/Navbar";
+import ImageEdit from "./components/ImageEdit"
 
 function App() {
   return (
     <NextUIProvider>
       <NavbarComponent />
+      <ImageEdit />
     </NextUIProvider>
   );
 }
