d0a67428536239daed1de87b6bd1cc314cd57eb8
Gulasal Kuchkarova <gulasalk424@gmail.com>
2023-03-22 09:34:20 +0100

    Create SideBar

--- src/components/Main/SideBar.js
+++ src/components/Main/SideBar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Button } from "@nextui-org/react";
-import {MdKeyboardArrowRight} from "react-icons/md"
-import "./SideBar.css"
+import { MdKeyboardArrowRight } from "react-icons/md";
+import "./SideBar.css";
 
 function SideBar() {
   return (
     <div className="SideBar">
-      <Button color="#E0FF03" css={{width:"98%", backgroundColor:"#E0FF03",marginLeft: "3px"}}>Persönliche Informationen<MdKeyboardArrowRight /></Button>
+      <Button
+        color="#E0FF03"
+        css={{ width: "98%", backgroundColor: "#E0FF03", marginLeft: "3px" }}
+      >
+        Persönliche Informationen
+        <MdKeyboardArrowRight />
+      </Button>
     </div>
   );
 }
